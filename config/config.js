a531442b35edecd760dd4336a23d56e7d197c909
Dimas-Mukhtar <95218465+Dimas-Mukhtar@users.noreply.github.com>
2024-12-16 22:02:30 +0700

    fixing database config 2

--- config/config.js
+++ config/config.js
@@ -18,13 +18,11 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    use_env_variable: process.env.DATABASE_URL, // Connection string untuk production
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: `${process.env.DB_NAME}_production`,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "postgres",
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
   },
 }
