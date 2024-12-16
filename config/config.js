0c591e2f435499a2481f003c2de04306ccb04075
Dimas-Mukhtar <95218465+Dimas-Mukhtar@users.noreply.github.com>
2024-12-16 22:54:14 +0700

    debugging

--- config/config.js
+++ config/config.js
@@ -18,11 +18,13 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: `${process.env.DB_NAME}_production`,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    use_env_variable: "DATABASE_URL", // Connection string untuk production
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 }
